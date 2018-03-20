25273c6941695a4baaa061345e17d1a228e89fb4
youpiyoful <yoanfornari@gmail.com>
2018-03-20 16:51:03 +0100

    déplacement fonctionnel

--- script.js
+++ script.js
@@ -1,64 +1,87 @@
 var player = document.getElementById("player1");
-var map;
+var map = document.getElementById("container");
 var blockHeight = player.offsetHeight;
 var blockWidth = player.offsetWidth;
-var x = 0;
-var y = 0;
+var posPlayerTop = player.offsetTop/50;
+console.log(posPlayerTop);
+var posPlayerLeft = player.offsetLeft/50;
+console.log(posPlayerLeft);
 var blockI;
 var block;
 
 
+var blockElt = [
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0],
+[0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+[0, 1, 2, 2, 0, 1, 0, 1, 0, 1, 2, 1, 0, 0],
+[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+[0, 1, 0, 1, 2, 1, 2, 2, 2, 1, 0, 1, 0, 0],
+[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0],
+[0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 0, 0],
+[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0],
+[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0],
+[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+
+
 document.addEventListener("keydown", function(e){
-    console.log(e.keyCode);
-    if((e.keyCode === 39) && (player.offsetLeft < map.offsetWidth - player.offsetWidth)&& (blockElt[x][y] == 1)){
-        player.style.left = player.offsetLeft + 10 + "px";
+
+    posPlayerTop = player.offsetTop/50;
+    posPlayerLeft = player.offsetLeft/50;
+
+    if((e.keyCode === 39) && (player.offsetLeft < map.offsetWidth - player.offsetWidth)){
+      console.log(blockElt[posPlayerTop][posPlayerLeft]);
+      if(blockElt[posPlayerTop][posPlayerLeft + 1] == 1){
+
+        player.style.left = (posPlayerLeft + 1) * 50 + "px";
+      }
     }
-    else if ((e.keyCode === 37) && (player.offsetLeft > 0)&& (blockElt[x][y] == 1)){
-      player.style.left = player.offsetLeft - 10 + "px";
+    else if ((e.keyCode === 37) && (player.offsetLeft > 0)){
+      console.log(blockElt[posPlayerTop][posPlayerLeft]);
+      if(blockElt[posPlayerTop][posPlayerLeft - 1] == 1){
+        posPlayerLeft -= 1;
+        player.style.left = player.offsetLeft - 50 + "px";
+      }
     }
-    else if ((e.keyCode === 40) && (player.offsetTop < map.offsetHeight - player.offsetHeight)&& (blockElt[x][y] == 1)) {
-      player.style.top = player.offsetTop + 10 + "px";
+    else if ((e.keyCode === 40) && (player.offsetTop < map.offsetHeight - player.offsetHeight)){
+      console.log(blockElt[posPlayerTop][posPlayerLeft]);
+      if(blockElt[posPlayerTop + 1][posPlayerLeft] == 1){
+          player.style.top = (posPlayerTop + 1) * 50 + "px";
+      }
     }
-    else if ((e.keyCode === 38) && (player.offsetTop > 0) && (blockElt[x][y] == 1)) {
-      player.style.top = player.offsetTop - 10 + "px";
-      console.log("coucou");
+    else if ((e.keyCode === 38) && (player.offsetTop > 0)) {
+      console.log(blockElt[posPlayerTop][posPlayerLeft]);
+      if(blockElt[posPlayerTop - 1][posPlayerLeft] == 1){
+        player.style.top = (posPlayerTop - 1) * 50 + "px";
+        console.log("coucou");
+      }
     }
     else if (e.keyCode === 32) {
       poseBombe();
     }
   });
 
-  var blockElt = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0],
-  [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
-  [0, 1, 2, 2, 0, 1, 0, 1, 0, 1, 2, 1, 0, 0],
-  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
-  [0, 1, 0, 1, 2, 1, 2, 2, 2, 1, 0, 1, 0, 0],
-  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
-  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0],
-  [0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 0, 0],
-  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0],
-  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
-  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0],
-  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-  ];
 
 
 
 function blockEltMaker(){
-  map = document.getElementById("container");
+  var x = 0;
+  var y = 0;
   for(x=0; x < 14; x++){
     for(y=0; y < 14; y++){
-      if (blockElt[x][y] == 0){
+      if (blockElt[y][x] == 0){
         blockI = document.createElement('div');
         blockI.classList.add("blockI");
         map.appendChild(blockI);
         blockI.style.top = y * blockHeight + "px";
         blockI.style.left = x * blockWidth + "px";
       }
-      else if (blockElt[x][y] == 2){
+      else if (blockElt[y][x] == 2){
         block = document.createElement("div");
         block.classList.add("block");
         block.style.top = y * blockHeight + "px";
@@ -72,8 +95,8 @@ function blockEltMaker(){
 function poseBombe(){
   var bombeElt = document.createElement("div");
   bombeElt.classList.add("bomb");
-  bombeElt.style.top = player.offsetTop + 25 + "px";
-  bombeElt.style.left = player.offsetLeft + 25 + "px";
+  bombeElt.style.top = player.offsetTop + 12.5 + "px";
+  bombeElt.style.left = player.offsetLeft + 12.5 + "px";
   map.appendChild(bombeElt);
 }
 
