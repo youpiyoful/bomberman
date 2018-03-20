8e3bf0b3152e2a80db72d7cef93e959ff14407b7
trustymalware <mehdi.benseghir@trustymalware.info>
2018-03-20 09:21:05 +0100

    poseBombe2

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var player = document.getElementById("player1");
-
+var map = document.getElementById("container");
 document.addEventListener("keydown", function(e){
-    console.log(e.keyCode);
-    if(e.keyCode === 39){
+    console.log(e.keyCode) 
+    if((e.keyCode === 39) && (player.offsetLeft < map.offsetWidth - player.offsetWidth)){
         player.style.left = player.offsetLeft + 10 + "px";
     }
 })
@@ -13,3 +13,4 @@ for (i=0; i < 10; i++){
     array.push(i);
 }
 console.log(array[length]);
+
