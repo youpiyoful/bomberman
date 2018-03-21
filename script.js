a6342fa61c47deed3111e9963b611331b5d25f33
youpiyoful <yoanfornari@gmail.com>
2018-03-21 12:36:48 +0100

    réorganisation du js 1: fonction 2: déclaration de variable 3: appelle des fonctions

--- script.js
+++ script.js
@@ -1,70 +1,3 @@
-var player = document.getElementById("player1");
-var map = document.getElementById("container");
-var blockHeight = player.offsetHeight;
-var blockWidth = player.offsetWidth;
-var posPlayerTop = player.offsetTop / 50;
-console.log(posPlayerTop);
-var posPlayerLeft = player.offsetLeft / 50;
-console.log(posPlayerLeft);
-var blockI;
-var block;
-var autoBomb = 0;
-
-var blockElt = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0],
-  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-  [0, 1, 2, 2, 0, 1, 0, 1, 0, 1, 2, 1, 1, 0],
-  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-  [0, 1, 0, 1, 2, 1, 2, 2, 2, 1, 0, 1, 1, 0],
-  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
-  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0],
-  [0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 0],
-  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0],
-  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
-  [0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0],
-  [0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
-
-
-
-document.addEventListener("keydown", function (e) {
-    posPlayerTop = player.offsetTop / 50;
-    posPlayerLeft = player.offsetLeft / 50;
-
-  if ((e.keyCode === 39) && (player.offsetLeft < map.offsetWidth - player.offsetWidth)) {
-    console.log(blockElt[posPlayerTop][posPlayerLeft]);
-    if (blockElt[posPlayerTop][posPlayerLeft + 1] == 1) {
-      player.style.left = (posPlayerLeft + 1) * 50 + "px";
-    }
-  } else if ((e.keyCode === 37) && (player.offsetLeft > 0)) {
-    console.log(blockElt[posPlayerTop][posPlayerLeft]);
-    if (blockElt[posPlayerTop][posPlayerLeft - 1] == 1) {
-      posPlayerLeft -= 1;
-      player.style.left = player.offsetLeft - 50 + "px";
-    }
-  } else if ((e.keyCode === 40) && (player.offsetTop < map.offsetHeight - player.offsetHeight)) {
-    console.log(blockElt[posPlayerTop][posPlayerLeft]);
-    if (blockElt[posPlayerTop + 1][posPlayerLeft] == 1) {
-      player.style.top = (posPlayerTop + 1) * 50 + "px";
-    }
-  } else if ((e.keyCode === 38) && (player.offsetTop > 0)) {
-    console.log(blockElt[posPlayerTop][posPlayerLeft]);
-    if (blockElt[posPlayerTop - 1][posPlayerLeft] == 1) {
-      player.style.top = (posPlayerTop - 1) * 50 + "px";
-      console.log("coucou");
-    }
-  } else if (e.keyCode === 32) {
-    if (autoBomb == 0) {
-      autoBomb = 1;
-      placeBomb();
-      setTimeout(explode, 1000);
-      autoBomb=0;
-    }
-  }
-});
-
 function blockEltMaker() {
   var x = 0;
   var y = 0;
@@ -86,35 +19,23 @@ function blockEltMaker() {
     }
   }
 }
-var bombeElt = document.createElement("div");
 
 function placeBomb() {
-
-  bombeElt.classList.add("bomb");
-  bombeElt.style.top = player.offsetTop + 5.5 + "px";
-  bombeElt.style.left = player.offsetLeft + 5.5 + "px";
-  map.appendChild(bombeElt);
-
-
-
+    var bombeElt = document.createElement("div");
+    bombeElt.classList.add("bomb");
+    bombeElt.style.top = player.offsetTop + 5.5 + "px";
+    bombeElt.style.left = player.offsetLeft + 5.5 + "px";
+    map.appendChild(bombeElt);
 }
 
 function explode() {
-
   bombeElt.classList.remove("bomb");
-
   bombeElt.style.top = bombeElt.offsetTop + 5.5 + "px";
   bombeElt.style.left = bombeElt.offsetLeft + 5.5 + "px";
-
-  delete map[posPlayerTop-1][posPlayerLeft];
+  // delete map[posPlayerTop-1][posPlayerLeft];
   // delete map[][blockWidth  ].object;
   // player.block++;
-
 }
-
-blockEltMaker();
-
-var monster = document.getElementById("phantom");
 
 function random() {
     var min = 1;
@@ -145,4 +66,70 @@ function random() {
 }
 
 
+var monster = document.getElementById("phantom");
+var player = document.getElementById("player1");
+var map = document.getElementById("container");
+var blockHeight = player.offsetHeight;
+var blockWidth = player.offsetWidth;
+var posPlayerTop = player.offsetTop / 50;
+console.log(posPlayerTop);
+var posPlayerLeft = player.offsetLeft / 50;
+console.log(posPlayerLeft);
+var blockI;
+var block;
+var autoBomb = 0;
+
+var blockElt = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+  [0, 1, 2, 2, 0, 1, 0, 1, 0, 1, 2, 1, 1, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+  [0, 1, 0, 1, 2, 1, 2, 2, 2, 1, 0, 1, 1, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0],
+  [0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+  [0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0],
+  [0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+blockEltMaker();
 setInterval(random, 200);
+document.addEventListener("keydown", function(e) {
+    posPlayerTop = player.offsetTop / 50;
+    posPlayerLeft = player.offsetLeft / 50;
+
+    if ((e.keyCode === 39) && (player.offsetLeft < map.offsetWidth - player.offsetWidth)) {
+        console.log(blockElt[posPlayerTop][posPlayerLeft]);
+        if (blockElt[posPlayerTop][posPlayerLeft + 1] == 1) {
+            player.style.left = (posPlayerLeft + 1) * 50 + "px";
+        }
+    } else if ((e.keyCode === 37) && (player.offsetLeft > 0)) {
+        console.log(blockElt[posPlayerTop][posPlayerLeft]);
+        if (blockElt[posPlayerTop][posPlayerLeft - 1] == 1) {
+            posPlayerLeft -= 1;
+            player.style.left = player.offsetLeft - 50 + "px";
+        }
+    } else if ((e.keyCode === 40) && (player.offsetTop < map.offsetHeight - player.offsetHeight)) {
+        console.log(blockElt[posPlayerTop][posPlayerLeft]);
+        if (blockElt[posPlayerTop + 1][posPlayerLeft] == 1) {
+            player.style.top = (posPlayerTop + 1) * 50 + "px";
+        }
+    } else if ((e.keyCode === 38) && (player.offsetTop > 0)) {
+        console.log(blockElt[posPlayerTop][posPlayerLeft]);
+        if (blockElt[posPlayerTop - 1][posPlayerLeft] == 1) {
+            player.style.top = (posPlayerTop - 1) * 50 + "px";
+            console.log("coucou");
+        }
+    } else if (e.keyCode === 32) {
+        if (autoBomb == 0) {
+            autoBomb = 1;
+            placeBomb();
+            setTimeout(explode, 1000);
+            autoBomb = 0;
+        }
+    }
+});
